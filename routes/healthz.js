16e83b2d37beb87c5be36505c42968ca8e6205b5
NishantGada <nishant.gada160@gmail.com>
2024-09-21 18:12:36 -0400

    setup: created routes directory with healthz route

---
+++ routes/healthz.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/healthz', async (req, res) => {
+
+    // adding response header to prevent caching
+    res.set('Cache-Control', 'no-cache');
+    // checking whether request contains any body, if so, returns 400 bad request error
+    if (Object.keys(req.body).length !== 0) {
+        return res.status(400).json()
+    } else {
+        // success case
+        return res.status(200).json({
+            "message": "Connection successful"
+        });
+    }
+})
+
+// returns 405 method not allowed in case any method apart from 'GET' is used
+router.all('/healthz', (req, res) => {
+    return res.status(405).json({
+        "message": `Method ${req.method} not allowed`
+    });
+});
+
+module.exports = router;
