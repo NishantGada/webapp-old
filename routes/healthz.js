958db4da89fcacb81e3f8fd50ca7f28818a98c34
NishantGada <nishant.gada160@gmail.com>
2024-09-25 18:33:12 -0400

    Added request-body check in healthz API

--- routes/healthz.js
+++ routes/healthz.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/healthz', async (req, res) => {
-
     // adding response header to prevent caching
     res.set('Cache-Control', 'no-cache');
     // checking whether request contains any body, if so, returns 400 bad request error
-    if (Object.keys(req.body).length !== 0) {
+    if (req.headers['content-length'] !== undefined) {
         return res.status(400).json()
     } else {
         // success case
