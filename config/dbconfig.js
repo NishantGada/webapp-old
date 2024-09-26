0c106e59259baa7749abb936c8e92151b28a9907
NishantGada <nishant.gada160@gmail.com>
2024-09-26 13:09:20 -0400

    change: switched from MySQL --> PostgreSQL + Added dialect to env variables

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -4,5 +4,5 @@ dotenv.config();
 import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: process.env.DB_DIALECT
 });
