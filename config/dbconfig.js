fbb93c8c539e63422fe6055c91fe09e8bb0e07a4
NishantGada <nishant.gada160@gmail.com>
2024-09-26 10:36:59 -0400

    update: replaced static values with env variables + removed unnecessary code

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,16 +1,8 @@
-// const { Sequelize } = require('sequelize');
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { Sequelize } from "sequelize";
-export const sequelize = new Sequelize('Cloud', 'root', 'gokuhinata1111', {
-    host: 'localhost',
+export const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 });
-
-const testConnection = async () => {
-    console.log("Inside testConnection");
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-};
