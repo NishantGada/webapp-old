589aa81f58a47287653d68c850fa89263caaf9e9
NishantGada <nishant.gada160@gmail.com>
2024-09-28 15:51:07 -0400

    update: Removed logging from sequelize console output

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -4,5 +4,6 @@ dotenv.config();
 import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    logging: false
 });
