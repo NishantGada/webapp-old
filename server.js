a96581bb39e32206552ae7669d3bf9cc6f7303fd
NishantGada <nishant.gada160@gmail.com>
2024-09-28 15:57:45 -0400

    update: Initial setup for bootstrapping DB in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import initialize from './app.js';
 import { sequelize } from './config/dbconfig.js';
+import { checkPassword, encryptPassword } from './utils/hash.js';
+import User from './models/user-model.js';
 
 const app = express();
 const port = process.env.PORT;
@@ -15,3 +17,17 @@ try {
 } catch (error) {
     console.error('Unable to connect to the database:', error);
 }
+
+/*
+const newHash = await encryptPassword("some-random-password");
+console.log("newHash: ", newHash);
+
+const passwordMatch = await checkPassword("some-random-password", newHash);
+console.log("passwordMatch: ", passwordMatch);
+*/
+
+sequelize.sync()
+    .then(() => {
+        console.log('User table has been successfully created, if one doesn\'t exist');
+    })
+    .catch(error => console.log('This error occurred:', error));
