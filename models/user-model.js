6cba3855fb72368232fd7fc37e2189b70c54aad0
NishantGada <nishant.gada160@gmail.com>
2024-09-28 15:47:43 -0400

    setup: Created new User model

---
+++ models/user-model.js
@@ -0,0 +1,39 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../config/dbconfig.js';  // Assuming dbconfig.js exports a configured Sequelize instance
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: true,  // Automatically adds createdAt and updatedAt fields
+    createdAt: 'accountCreated',
+    updatedAt: 'accountUpdated',
+    defaultScope: {
+        attributes: { exclude: ['password'] }  // Exclude 'password' by default in all queries
+    }
+});
+
+export default User;
