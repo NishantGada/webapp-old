105248a5699bf41fdde9bfa1c02ab42bd2392a39
NishantGada <nishant.gada160@gmail.com>
2024-10-02 19:41:47 -0400

    cleanup: Removed unnecessary/ commented code from server.js file

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import initialize from './app.js';
 import { sequelize } from './config/dbconfig.js';
-import { checkPassword, encryptPassword } from './utils/hash.js';
-import User from './models/user-model.js';
 
 const app = express();
 const port = process.env.PORT;
@@ -18,16 +16,8 @@ try {
     console.error('Unable to connect to the database:', error);
 }
 
-/*
-const newHash = await encryptPassword("some-random-password");
-console.log("newHash: ", newHash);
-
-const passwordMatch = await checkPassword("some-random-password", newHash);
-console.log("passwordMatch: ", passwordMatch);
-*/
-
 sequelize.sync()
     .then(() => {
-        console.log('User table has been successfully created, if one doesn\'t exist');
+        console.log('User table has been successfully created');
     })
     .catch(error => console.log('This error occurred:', error));
