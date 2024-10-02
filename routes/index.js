4ed4dd5b7fcc3ce8f97dcb66c033b6995e3d33bb
NishantGada <nishant.gada160@gmail.com>
2024-10-02 02:33:53 -0400

    update: Defined a common route for all User APIs

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ import userRouter from "./user-route.js"
 export default (app) => {
     app.use('/healthz', healthzRouter);
     
-    app.use('/users', userRouter);
-    app.use('/create-new-user', userRouter);
+    // Defined a common route for all User APIs. Differentiation would be on the basis of method types - GET, POST, PUT, etc
+    app.use('/v1/user', userRouter);
 }
