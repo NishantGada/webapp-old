db6c7213b45ce80544a7214bfeefd7306c237189
NishantGada <nishant.gada160@gmail.com>
2024-10-02 02:39:27 -0400

    update: Handled User routes individually based on methods and paths

--- routes/user-route.js
+++ routes/user-route.js
@@ -3,9 +3,18 @@ import * as userController from '../controllers/user-controller.js';
 
 const router = express.Router();
 
-// Route to fetch all users
-router.route("/")
+// Controller mappings for respective APIs
+router.route("/self")
     .get(userController.getAllUsers)
+    .put(userController.updateUserDetails)
+    .head(userController.handleHeadMethod)
+
+router.route("/")
     .post(userController.createNewUser)
+
+// Login to block all other methods that haven't been handled, except "HEAD"
+router.all("/", (req, res) => {
+    res.status(405).json();
+});
 
 export default router;
