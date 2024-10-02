4911204a546a4208d05ee3eaf08a3b4b7a4c6cdd
NishantGada <nishant.gada160@gmail.com>
2024-10-02 02:34:52 -0400

    update: Modified route for healthz API

--- routes/healthz-route.js
+++ routes/healthz-route.js
@@ -3,17 +3,17 @@ import * as healthzController from '../controllers/healthz-controller.js';
 
 const router = express.Router();
 
-router.get('/healthz', (req, res) => {
+router.get('/', (req, res) => {
     healthzController.get(req, res);
 });
 
 // Middleware logic to disallow URL parameters
-router.use('/healthz/:param', (req, res) => {
+router.use('/:param', (req, res) => {
     res.status(400).json();
 });
 
 // Used to catch all for unsupported methods
-router.all("/healthz", (req, res) => {
+router.all("/", (req, res) => {
     res.status(405).json();
 });
 
