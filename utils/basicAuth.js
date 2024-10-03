8c1976a231d4d14d1825154159cb30a8badce530
NishantGada <nishant.gada160@gmail.com>
2024-10-02 20:04:58 -0400

    update: Added missing username/password check in Basic Auth

--- utils/basicAuth.js
+++ utils/basicAuth.js
@@ -1,18 +1,21 @@
 export const validateBasicAuth = (req, res) => {
-    console.log("Inside validateBasicAuth");
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith('Basic ')) {
-        return res.status(401).json({ message: 'Missing or invalid authorization header' });
+        console.log("Missing or invalid authorization header");
+        return res.status(401).json();
     }
 
     // Decode the Base64 credentials
     const base64Credentials = authHeader.split(' ')[1];
-    
+
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [username, password] = credentials.split(':');
 
-    console.log("username, password: ", username, password);
+    if (!username || !password) {
+        console.log("401 status");
+        return ["", "", false];
+    }
 
-    return [username, password];
+    return [username, password, true];
 }
